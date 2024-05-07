eb71c6fc747c109745d4f85f03ca8def1fbd340b
Josestac10 <105133848+Josestac10@users.noreply.github.com>
2024-05-07 12:11:58 -0500

    hola
    hOASTING

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config';
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind()],
+  server:{
+    port: 8082,
+    open: true,
+  },
+});
